7f52c506c2edd25ceb3445560ea4ac1b29719ebe
AgungSukaAFK <wismaagung23@gmail.com>
2025-05-15 17:20:41 +0700

    Remove prisma

--- app/api/mahasiswa/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
-
-export async function GET() {
-  const data = await prisma.mahasiswa.findMany();
-  return NextResponse.json({
-    data: data,
-  });
-}
